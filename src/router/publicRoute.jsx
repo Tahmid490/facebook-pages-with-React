05761ecdddc3c16c498a055a0fdca0eff2988007
Tahmid490 <tahmid1526@gmail.com>
2023-12-12 02:32:13 -0500

    Project init setup done

---
+++ src/router/publicRoute.jsx
@@ -0,0 +1,19 @@
+//create public router
+
+import Auth from "../pages/auth/Auth";
+import Home from "../pages/home/Home";
+
+const publicRouter = [
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/auth",
+    element: <Auth />,
+  },
+];
+
+//export
+
+export default publicRouter;
